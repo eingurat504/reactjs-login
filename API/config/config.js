cee08cf38bd2bb2f7c0f40494601923c172f1f87
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-26 08:47:43 +0300

    modified to config.js

--- API/config/config.js
+++ API/config/config.js
@@ -1,46 +1,12 @@
-'use strict';
+const { Client }= require('pg');
+const env = require('dotenv').config();
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
-const config = require(__dirname + '/../config/config.js');
-const db = {};
-
-const sequelize = new Sequelize(config.database, config.user, config.password, {
-    host: config.host,
-    dialect: 'postgres',
-    port: config.port,
-    operatorsAliases: false
-    // pool: {
-    //   max: config.pool.max,
-    //   min: config.pool.min,
-    //   acquire: config.pool.acquire,
-    //   idle: config.pool.idle
-    // }
-  });
-  
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+const config = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-db.projects = require("../models/project.js")(sequelize, Sequelize);
-db.tasks = require("../models/task.js")(sequelize, Sequelize);
-db.users = require("../models/user.js")(sequelize, Sequelize);
-
-module.exports = db;
+module.exports = config;
